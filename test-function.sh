32582e5f78a52c1aae83cab82ebe6803c437ff70
Yasser Saied <ysaied80@yahoo.com>
2018-10-10 13:03:39 +0200

    update

--- test-function.sh
+++ test-function.sh
@@ -2,5 +2,20 @@
 
 
 
+#apt-cache show python | grep "Package: python"
 
+function install_linux_package()
+{
+   check_package="$(apt-cache show $1 | grep \"Package: $1\")" &> /dev/null
+   if [ -z $check_package ]
+   then
+      echo "$1 Installed"
+   else
+      (sudo apt-get install -y $1 &> /dev/null)
+      echo "$1 Installed"
+   fi
+
+}
+
+install_linux_package "default-jre"
 
