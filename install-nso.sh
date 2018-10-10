19a792785a4727031101aae8986482bc39588882
Yasser Saied <ysaied80@yahoo.com>
2018-10-10 10:18:01 +0200

    update

--- install-nso.sh
+++ install-nso.sh
@@ -48,93 +48,47 @@ echo ""
 sudo apt-get -y update > /dev/null
 sudo apt-get -y upgrade > /dev/null
 
-check_package="$(apt list --installed show default-jre)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Default JRE Installed"
-else
-   (sudo apt-get install -y default-jre > /dev/null)
-fi
 
-check_package="$(apt list --installed show openjdk-11-jre-headless)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Open JDK 11 Installed"
-else
-   (sudo apt-get install -y openjdk-11-jre-headless > /dev/null)
-fi
+function install_linux_package()
+{
+   check_package="$(apt list --installed show $1)" &> /dev/null
+   if [[ $check_package == *"installed"* ]]
+   then
+      echo "$1 Installed"
+   else
+      (sudo apt-get install -y $1 &> /dev/null)
+   fi
 
-check_package="$(apt list --installed show openjdk-8-jre-headless)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Open JDK 8 Installed"
-else
-   (sudo apt-get install -y openjdk-8-jre-headless > /dev/null)
-fi      
+}
 
-check_package="$(apt list --installed show expect)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Expect Installed"
-else
-   (sudo apt-get install -y expect > /dev/null)
-fi 
+function install_python_package()
+{
+   check_package="$(pip freeze | grep $1)"
+   if [[ "$check_package" ]]
+   then
+      echo "$1 Python Package Installed"
+   else
+      (pip install $1 &> /dev/null)
+   fi
 
-check_package="$(apt list --installed show sshpass)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "SSH Pass Installed"
-else
-   (sudo apt-get install -y sshpass > /dev/null)
-fi 
+}
 
-check_package="$(apt list --installed show python)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Python 2.7 Installed"
-else
-   (sudo apt-get install -y python > /dev/null)
-fi 
+#linux packages
+install_linux_package "default-jre"
+install_linux_package "openjdk-11-jre-headless"
+install_linux_package "openjdk-8-jre-headless"
+install_linux_package "ant" 
+install_linux_package "expect"
+install_linux_package "sshpass"
+install_linux_package "python"
+install_linux_package "python3"
+install_linux_package "python-pip" 
 
-check_package="$(apt list --installed show python3)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Python 3.6 Installed"
-else
-   (sudo apt-get install -y python3 > /dev/null)
-fi 
+#python packages
+install_python_package "paramiko"
+install_python_package "netmiko"
+install_python_package "ncs"
 
-check_package="$(apt list --installed show python-pip)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Python PIP Installed"
-else
-   (sudo apt-get install -y python-pip python3-pip > /dev/null)
-fi 
-
-check_package="$(pip freeze | grep paramiko)"
-if [[ "$check_package" ]]
-then 
-   echo "Paramiko Python Package Installed"
-else
-   (pip install Paramiko > /dev/null)
-fi 
-
-check_package="$(pip freeze | grep ncs)"
-if [[ "$check_package" ]]
-then 
-   echo "NCS Python Package Installed"
-else
-   (pip install ncs > /dev/null)
-fi 
- 
-check_package="$(apt list --installed show ant)"
-if [[ $check_package == *"installed"* ]]
-then 
-   echo "Ant Installed"
-else
-   (sudo apt-get install -y ant > /dev/null)
-fi 
 
 echo ""
 echo "##########################################"
@@ -176,30 +130,34 @@ echo ""
 
 rm -rf $NCS_DIR/packages/*
 
-
 function install_ned()
 {
-	(cd /var/tmp/ncs-downloads && sh $2) > /dev/null
-	(cd /var/tmp/ncs-downloads && tar -xf $3) > /dev/null
-	(cp -r /var/tmp/ncs-downloads/$1 $NCS_DIR/packages) > /dev/null
-	(cd $NCS_DIR/packages/$1/src && make) > /var/tmp/make-$1
-    (cd ~/ncs-run/packages/ && ln -s $NCS_DIR/packages/$1)
-	if grep -q "BUILD SUCCESSFUL" /var/tmp/make-$1
-	then
-    	echo "$1 NED extracted successfully :-)"
-	else
-		echo "$1 NED extraction failed :-("
+   (cd /var/tmp/ncs-downloads && sh $2) > /dev/null
+   (cd /var/tmp/ncs-downloads && tar -xf $3) > /dev/null
+   (cp -r /var/tmp/ncs-downloads/$1 $NCS_DIR/packages) > /dev/null
+   (cd $NCS_DIR/packages/$1/src && make) &> /var/tmp/make-$1
+   (cd ~/ncs-run/packages/ && ln -s $NCS_DIR/packages/$1)
+   if grep -q "BUILD SUCCESSFUL" /var/tmp/make-$1
+   then
+      echo "$1 NED extracted successfully :-)"
+   else
+      echo "$1 NED extraction failed :-("
 	fi
 }
 
+#cisco-ios ned
 install_ned "cisco-ios" "ncs-4.7.1-cisco-ios-6.4.1.signed.bin" "ncs-4.7.1-cisco-ios-6.4.1.tar.gz"
 
+#cisco-iosxr ned
 install_ned "cisco-iosxr" "ncs-4.7-cisco-iosxr-7.3.2.signed.bin" "ncs-4.7-cisco-iosxr-7.3.2.tar.gz"
 
+#juniper-junos ned
 install_ned "juniper-junos" "ncs-4.7.1-juniper-junos-4.0.4.signed.bin" "ncs-4.7.1-juniper-junos-4.0.4.tar.gz"
 
+#alcatel sr ned
 install_ned "alu-sr" "ncs-4.7-alu-sr-7.10.signed.bin" "ncs-4.7-alu-sr-7.10.tar.gz"
 
+#viptela vManage ned
 install_ned "viptela-vmanage" "ncs-4.7-viptela-vmanage-1.2.2.signed.bin" "ncs-4.7-viptela-vmanage-1.2.2.tar.gz"
 
 
